9b30278e2e3d94ddd0e5bce98a10f530bc7edf3e
Pixako <sorawis2546@gmail.com>
2023-11-16 20:08:25 +0900

    fixed add assignment

--- api.py
+++ api.py
@@ -121,15 +121,15 @@ async def get_by_month(request: Request, yyyymm: str, email: str = Depends(is_lo
 
 @app.get("/{email}/assignments/{date}", response_class=HTMLResponse)
 async def get_assignments(request: Request, date: str, email: str = Depends(is_logged_in)):
-    assignments = []
-    if date in root.assignments:
-        for assignment in root.assignments[date]:
-            assignments.append({"subject": assignment.subject, "name": assignment.name})
-    return templates.TemplateResponse("assignment.html", {"request": request, "email": email, "assignments": assignments, "date": date})
+	can_edit : bool = root.students[email].edit
+	assignments = []
+	if date in root.assignments:
+		for assignment in root.assignments[date]:
+			assignments.append({"subject": assignment.subject, "name": assignment.name})
+	return templates.TemplateResponse("assignment.html", {"request": request, "email": email, "assignments": assignments, "date": date, "can_edit": can_edit})
 
 @app.post("/{email}/assignments/{date}", response_class=HTMLResponse)
 async def add_assignment(request: Request, date: str, assignment_name: str = Form(...), subject: str = Form(...), content: str = Form(...), email: str = Depends(is_logged_in)):
-    can_edit = root.students[email].edit
     if date not in root.assignments:
         root.assignments[date] = PersistentList()
     new_assignment = Assignment(assignment_name, subject, date, content, email)
@@ -139,7 +139,7 @@ async def add_assignment(request: Request, date: str, assignment_name: str = For
     assignments = []
     for assignment in root.assignments[date]:
         assignments.append({"subject": assignment.subject, "name": assignment.name})
-    return templates.TemplateResponse("assignment.html", {"request": request, "email": email, "assignments": assignments, "date": date, "can_edit": can_edit})
+    return templates.TemplateResponse("assignment.html", {"request": request, "email": email, "assignments": assignments, "date": date})
 
 @app.get("/{email}/assignments/{date}/{assignment_index}", response_class=HTMLResponse)
 async def get_assignment(request: Request, date: str, assignment_index: int, email: str = Depends(is_logged_in)):
